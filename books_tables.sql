60e7ad85faa5b1e44ddc2f64d120185b7e6670b0
Andrea Morgan <morgan.andrea.8984@gmail.com>
2019-08-22 21:23:40 -0500

    Adding sql events table schema

--- books_tables.sql
+++ books_tables.sql
@@ -4,3 +4,18 @@ CREATE TABLE books (
 	title varchar(1000),
 	authors varchar(1000)
 );
+
+-- Events Table
+CREATE TABLE events (
+	event_id int PRIMARY KEY NOT NULL,
+	event_source varchar(500),
+	event_isbn bigint references books(book_id),
+	event_participants varchar(500),
+	event_date varchar(25),
+	event_location varchar(500),
+	event_city varchar(100),
+	event_state varchar(100),
+	event_description varchar(1000)
+	event_market varchar(100)
+
+)
